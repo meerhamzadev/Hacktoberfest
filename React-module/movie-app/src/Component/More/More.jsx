8c66f901fbf8452918fa6634875e7d5a71055594
akshay-Kalekar <akalekar2003@gmail.com>
2022-10-05 00:34:58 +0530

    Created Component for smooth coding

---
+++ React-module/movie-app/src/Component/More/More.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const More = () => {
+  return (
+    <div>More</div>
+  )
+}
+
+export default More
---
+++ React-module/movie-app/src/Component/Subscription/Subscription.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Subscription = () => {
+  return (
+    <div>Subscription</div>
+  )
+}
+
+export default Subscription
